9f2455b600dc33d866f9aab9af3140fdcf91aeff
phucquangne <phucquang2001@gmail.com>
2022-11-05 14:45:37 +0700

    upload

---
+++ JS/custom.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+	$('#ban').owlCarousel({
+    loop:true,
+   
+    nav:true,
+    items:1,
+ })  
+})// JavaScript Document
+
+
+
+
+
+$(document).ready(function(){
+	$('#combo').owlCarousel({
+    loop:true,
+    margin:60,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+})// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function(){
+    // make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+    
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+    
+            everyitem.addEventListener('mouseover', function(e){
+    
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+    
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                let el_link = this.querySelector('a[data-bs-toggle]');
+    
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+    
+    
+            })
+        });
+    
+    }
+    // end if innerWidth
+    }); 
+    // DOMContentLoaded  end
+
+    $(document).ready(function() {
+        $('.minus').click(function () {
+            var $input = $(this).parent().find('input');
+            var count = parseInt($input.val()) - 1;
+            count = count < 1 ? 1 : count;
+            $input.val(count);
+            $input.change();
+            return false;
+        });
+        $('.plus').click(function () {
+            var $input = $(this).parent().find('input');
+            $input.val(parseInt($input.val()) + 1);
+            $input.change();
+            return false;
+        });
+    });
